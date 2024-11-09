0353d919e429881793ceaea639162a2e32816c7f
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-11-09 19:20:40 +0500

    Add files via upload

---
+++ hackathon/app/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='w-screen h-8 bg-black text-white font-bold flex justify-center items-center text-[10px]'>
+        Footer - Created By Abubakar Chohan
+    </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ hackathon/app/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='w-screen h-8 bg-black text-white font-bold flex justify-center items-center text-xl'>
+        CV / Resume Website
+    </div>
+    </>
+  )
+}
+
+export default Navbar
